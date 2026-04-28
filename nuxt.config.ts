8d30b89a96070b4a78860cd857addbfe01c21b05
André <aws@umbraco.dk>
2026-04-28 13:51:38 +0200

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,9 @@ export default defineNuxtConfig({
         },
         { name: 'theme-color', content: '#0b1320' },
       ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
     },
   },
 
